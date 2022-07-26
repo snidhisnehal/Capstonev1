de89511cbd12d866d2bb2c2499d18858ee725070
Snehal Bhamare <Snehal.Bhamare@ezest.local>
2022-07-26 09:47:40 +0530

    React capstonev1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import '../src/Scss/Library/index.css'
+import { Routes,Route } from 'react-router-dom';
+import Home from '../src/Pages/Home/home'
+import ProductListing from '../src/Pages/ProductListing/productisting'
+import Navbar from '../src/Components/Header/navbar'
+import Footer from '../src/Components/Footer/mainfooter'
+import SubFooter from '../src/Components/Footer/lowerfooter'
+import SingleProduct from './Pages/ProductDetails/SinglePrduct';
+import CartItems from './Pages/Cart/CartItems';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/product" element={<ProductListing />} /> 
+        <Route exact path="/products/:id" element={<SingleProduct />} />
+        <Route exact path="/cart" element={<CartItems/>} />  
+      </Routes> 
+     <Footer/>
+     <SubFooter/>
     </div>
   );
 }
